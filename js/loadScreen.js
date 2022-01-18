55ac622ad0d515a9af62ae0a4176580cebb136e2
Josh Cunningham <josh@joshcanhelp.com>
2022-01-17 20:02:53 -0800

    refactor: clean up route handling

--- js/loadScreen.js
+++ js/loadScreen.js
@@ -1,22 +1,18 @@
 const appScreen = document.getElementById("app-screen");
 const pageTitle = document.getElementsByTagName("title")[0];
-""
+
 export const loadScreen = async (screenName = "") => {
+  // Clear out existing screen content
   appScreen.innerHTML = "";
 
   // Set the screen to home if path is empty
   screenName = screenName.slice(1) || "home";
 
+  const handler = await import(`./handlers/${screenName}.js`);
+
   // Set the page title or a default
-  pageTitle.text = (screenTitles[screenName] ? `${screenTitles[screenName]} | ` : "") + "AppName";
+  pageTitle.text = handler.getTitle() + " | AppName";
 
   // Get the view we want to display
-  const response = await fetch(`../views/${screenName}.html`);
-  appScreen.innerHTML = await response.text();
-};
-
-const screenTitles = {
-  "home": "Home",
-  "posts": "Your Posts",
-  "create": "Create a Post",
+  appScreen.innerHTML = await handler.getView();
 };

23b59403df8782fb4e2f352bc30531bf260b1196
Josh Cunningham <josh@joshcanhelp.com>
2022-01-18 21:06:13 -0800

    fix: tailwind build nav active

--- js/loadScreen.js
+++ js/loadScreen.js
@@ -1,22 +1,34 @@
 const appScreen = document.getElementById("app-screen");
 const pageTitle = document.getElementsByTagName("title")[0];
 
-export const loadScreen = async (screenName = "") => {
+export const loadScreen = async (pathname = "") => {
+  appScreen.classList.add("loading");
+
   // Clear out existing screen content
   appScreen.innerHTML = "";
 
   // Set the screen to home if path is empty
-  screenName = screenName.slice(1) || "home";
+  const screenName = pathname.slice(1) || "home";
 
-  const handler = await import(`./handlers/${screenName}.js`);
+  const { getTitle, getView, postRender } = await import(`./handlers/${screenName}.js`);
 
   // Set the page title or a default
-  pageTitle.text = handler.getTitle() + " | AppName";
+  pageTitle.text = getTitle() + " | AppName";
 
   // Get the view we want to display
-  appScreen.innerHTML = await handler.getView();
+  appScreen.innerHTML = await getView();
+  
+  appScreen.classList.remove("loading");
 
-  if (typeof handler.postRender === "function") {
-    await handler.postRender();
+  if (typeof postRender === "function") {
+    await postRender();
   }
+
+  document.querySelectorAll("nav a").forEach((el) => {
+    if (el.getAttribute("href") === pathname) {
+      el.classList.add("active");
+    } else {
+      el.classList.remove("active");
+    }
+  });
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./views/**/*.html"
+    "./views/**/*.html",
+    "./js/**/*.js"
   ]
 }

62a6b6b987aa9274193fb0975caf6ed80a38a787
Josh Cunningham <josh@joshcanhelp.com>
2022-01-31 20:15:42 -0800

    feat: add dynamic interface module

---
+++ src/dynamicInterface.js
@@ -0,0 +1,12 @@
+import { isAuthenticated } from "./helpers.js";
+
+export const loadDynamicInterface = async () => {
+  const appScreen = document.getElementById("account-links");
+
+  if (isAuthenticated()) {
+    const logoutLink = document.createElement("a");
+    logoutLink.textContent = "Logout";
+    logoutLink.setAttribute("href", "/logout");
+    appScreen.prepend(`You're logged in as ${localStorage.getItem("name")} - `, logoutLink);
+  }
+};

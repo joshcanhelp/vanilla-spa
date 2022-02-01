f30d2b0605eaff587fa53cdae4c1047e698a2107
Josh Cunningham <josh@joshcanhelp.com>
2022-01-31 20:15:36 -0800

    feat: add dynamic interface module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { registerRouter } from "./router.js";
+import { loadDynamicInterface } from "./dynamicInterface.js";
 
 const app = async () => {
   console.log("App loading ...");
   registerRouter();
+  loadDynamicInterface();
 };
 
 document.addEventListener("DOMContentLoaded", app);
